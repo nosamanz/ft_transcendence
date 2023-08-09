567968b54a92f1cc014e2f5085e4367fab428ac0
Osman <oozcanosman@gmail.com>
2023-08-09 11:49:24 +0300

    db_start

--- NestJS/src/users/users.controller.ts
+++ NestJS/src/users/users.controller.ts
@@ -1,15 +1,19 @@
-import { Controller, Get, Param, Post , Body} from '@nestjs/common';
+import { Controller, Get, Param, Post , Body, Res} from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { User} from './entitiest/user.entity';
+import { Response } from 'express';
+import * as path from 'path'
 
 @Controller('users')
 export class UsersController {
 	constructor(private usersService: UsersService){}
 
 	@Get()
-	getUsers(){
+	getUsers(@Res() response : Response){
 		this.usersService.findAll();
+		const filePath = path.join(__dirname, '..', '..' ,'..', 'index', 'index.html');
+		return response.sendFile(filePath);
 	}
 	@Get(':id')
 	getUserById(@Param('id') id: string): User { // TODO : auto parse ID
