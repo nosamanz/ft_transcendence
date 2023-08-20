98ddb7131a0dd30cf8bef5f82290a81b93283501
Esma Battal <ebattal@k2m14s05.42kocaeli.com.tr>
2023-08-20 17:16:31 +0300

    fronted_new

--- Front/JavaScript/index.js
+++ Front/JavaScript/index.js
@@ -1,17 +1,15 @@
 
-function showContent(e) {
-	e.preventDefault();
-	alert("hi");
-	return("dasdas");
-	var contentContainer = document.getElementById("home");
-	var id = document.getElementById("id").value;
-	var pass = document.getElementById("pass").value;
-	if (id == "esma" && pass == "123")
-	{
-		contentContainer.classList.add("active");
-		hiddenLogin();
-	}
+function showContent() {
 
+	var contentContainer = document.getElementById("id");
+	// var id = document.getElementById("id").value;
+	// var pass = document.getElementById("pass").value;
+	// if (id == "esma" && pass == "123")
+	// {
+	// 	contentContainer.classList.add("active");
+	// 	hiddenLogin();
+	// }
+	hiddenLogin();
 	
 }
 
@@ -20,3 +18,35 @@ function hiddenLogin()
 	var loginContainer = document.getElementById("login");
 	loginContainer.classList.add("pasive");
 }
+
+function hiddenHeader()
+{
+	var headerContainer = document.getElementById("first_header");
+
+}
+
+function showHeader()
+{
+
+}
+
+function lines()
+{
+	let sizeW = Math.random() * 12;
+	let e = document.createElement('div');
+	e.setAttribute('class', 'circle');
+	document.body.appendChild(e);
+
+	e.style.width= 2 +sizeW+'px';
+	e.style.left = Math.random() * + innerWidth + 'px';
+	setTimeout(function()
+	{
+		document.body.removeChild(e)
+	}, 5000);
+}
+
+
+setInterval(function()
+{
+	lines();
+}, 1000);
